4619a0328686c1d856246356485b97ec44642b0c
soohyeon <meltapplee@gmail.com>
2025-07-15 13:51:29 +0900

    feat :: post, comment 엔티티

---
+++ src/entities/Comment.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+} from "typeorm";
+import { Post } from "./Post";
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column("text")
+  content!: string;
+
+  // @ManyToOne(() => User, (user) => user.comments)
+  // author: User; // 작성자
+
+  @ManyToOne(() => Post, (post) => post.comments)
+  post!: Post;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
