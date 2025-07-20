d1f4397c9a9f6525bc3fd729d6422a65586383bf
soohyeon <meltapplee@gmail.com>
2025-07-20 22:34:23 +0900

    chore :: gpt API 주석 삭제

--- src/controllers/job/jobController.ts
+++ src/controllers/job/jobController.ts
@@ -1,14 +1,14 @@
-// import { TalkRequest } from "../../dtos/job/request/jobRequest";
-// import { openAIWithUserInput } from "../../services/job/gptService";
-// import { Request, Response } from "express";
+import { TalkRequest } from "../../dtos/job/request/jobRequest";
+import { openAIWithUserInput } from "../../services/job/gptService";
+import { Request, Response } from "express";
 
-// export class JobController {
-//   async talk(req: Request<{}, {}, TalkRequest>, res: Response) {
-//     try {
-//       const result = await openAIWithUserInput(req.body);
-//       res.status(200).json(result);
-//     } catch (error) {
-//       res.status(500).json({ message: "요청 처리에 실패했습니다." });
-//     }
-//   }
-// }
+export class JobController {
+  async talk(req: Request<{}, {}, TalkRequest>, res: Response) {
+    try {
+      const result = await openAIWithUserInput(req.body);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: "요청 처리에 실패했습니다." });
+    }
+  }
+}
--- src/routes/job/jobRouter.ts
+++ src/routes/job/jobRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-// import { JobController } from "../../controllers/job/jobController";
+import { JobController } from "../../controllers/job/jobController";
 
 const jobRouter = Router();
-// const jobController = new JobController();
+const jobController = new JobController();
 
-// jobRouter.post("/job/talk", jobController.talk.bind(jobController));
+jobRouter.post("/job/talk", jobController.talk.bind(jobController));
 
 export default jobRouter;
